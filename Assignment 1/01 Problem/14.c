6e4a7e1ef2f663a2ba023cf4bc73278d0353f4e0
Arman Bin Alauddin <160709760+armanbin007@users.noreply.github.com>
2024-12-26 01:52:26 +0600

    Solved in a easier way
    Used function previously.

--- Assignment 1/01 Problem/14.c
+++ Assignment 1/01 Problem/14.c
@@ -1,19 +1,9 @@
 #include <stdio.h>
-
-int a = 10; // Global value
-void print(); // Function prototype
-
-void print()  // Defining function
-{  
-    printf("C. Global: %d\n", a);
-}
-
 int main(){
-    printf("A. Global: %d\n", a);
+    int a = 10;
+    printf("A. Global: %d\n", a); {
     int a = 20; // Local value
-    printf("B. Local: %d\n", a);
-    print();
-    
-
+    printf("B. Local: %d\n", a);}
+    printf("C. Global: %d\n", a);
     return 0;
 }
